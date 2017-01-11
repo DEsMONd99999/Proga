6486784db1b0df862a3157d6faf2c0ec8d1b0c9c
DEsMONd99999 <desmond99999@mail.ru>
2017-01-11 03:28:38 +0300

    Add files via upload

---
+++ Круг в квадрат.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int x, a, r;
+	cout << "Что сделать:" << endl << "1. Поместить круг в квадрат" << endl << "2. Поместить квадрат в круг";
+	cin >> x;
+	cout << "Введите сторону квадрата" << endl;
+	cin >> a;
+	cout << "Введите радиус круга " << endl;
+	cin >> r;
+	if (x == 1) {
+		if (a >= r*r) cout << "Круг поместится в квадрате";
+		else  cout << "Круг не поместится в квадрате";
+		cout << endl;
+	}
+	if (x == 2) {
+		if ((((sqrt(2))*a)/2)<=r) cout << "Квадрат поместится в круге";
+		else  cout << "Квадрат не поместится в круге";
+		cout << endl;
+	}	
+	system("pause");
+	return 0;
+}
