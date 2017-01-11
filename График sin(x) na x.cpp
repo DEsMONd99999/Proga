47fc02b035f2035e842f439d8408cd5d1ddff757
DEsMONd99999 <desmond99999@mail.ru>
2017-01-11 03:16:45 +0300

    Add files via upload

---
+++ График sin(x) na x.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream> 
+#include <iomanip> 
+int main()
+{
+	float x = -360, y;
+	while (x < 360)
+	{
+		y = ((sin(x * 3.14 / 90) * 80) / x * 20);
+		std::cout << std::setw(40 + (y)) << std::setfill(' ') << '*' << "\n";
+		x = x + 5;
+		if (x == 0)
+		{
+			x = x + 10;
+		}
+		y++;
+	}
+	system("pause");
+}
